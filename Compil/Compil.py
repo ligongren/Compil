bc7d8b0025c6e8e664837c8bf45ece572d298fc9
he <928585672@qq.com>
2017-09-12 01:21:35 +0800

    完成左递归消除

--- Compil/Compil.py
+++ Compil/Compil.py
@@ -1,15 +1,34 @@
- 
+def simpli(number):
+    G_simed.append(G_format[number][0])
+    for i in G_format[number][1:]:
+        if not i in G_simed and (i.islower() or G[number][0] in i):
+            G_simed.append(i)
 
 def remove_direc():
-    G.append('dir')
+    G_output.append(G_simed[0])
+    G_none.append(G_simed[0] + "'")
+    for i in G_simed[1:]:
+        if i.islower():
+            G_none.append(i + G_simed[0] + "'")
+        else:
+            G_output.append(i[1:]+i[0]+"'")
+    G_output.append("ε")
 
-
-def remove_indir():
-    G.append('in')
-
-
+def remove_indir(i,j):
+    # 循环每个Pi的元素
+    for now in range(1,len(G_format[i])):
+        # 若此元素是从Pi→Pj
+        if G_format[j][0] in G_format[i][now]:
+            # 记录γ
+            gama = G_format[i][now][1:]
+            # 向Pi中添加δ1γ
+            for jNum in range(len(G_format[j])):
+                G_format[i].append(G_format[j][jNum] + gama)
+            # 删除原有的Pjγ
+            del G_format[i][now]
+    # 删除原有的Pjγ的后，后添加的规则就会被循环遍历到。会假如重复结果。
+    # 只需在简化时删除即可
 def format(number):
-
     lastAlp = 0
     now = 0
     while now < len(G[number]):
@@ -19,36 +38,31 @@ def format(number):
         now+=1
     G_format[number].append(G[number][lastAlp:now])
 
-    
-    #for i in range(1,len(G[number])):
-    #    if G[number][i].isalpha() and i != len(G[number]) - 1:
-    #        j = i
-    #        while G[number][j + 1].isalpha():
-    #            if j == len(G[number]):
-    #                break
-    #            j+=1
-    #        if i != j:
-    #            G_format[number].append(G[number][i:j + 1])
-    #            i = j + 1
-    #    if i == len(G[number]) - 1:
 
-
-
-n = input('请输入文法的数量n：')
+# n = input('请输入文法的数量n：')
+n = 3
 n = int(n)
-print('文法格式为：S:Qc|c')
-G = []
+# print('文法格式为：S:Qc|c')
+# G = []
+G = ['R:Sa|a','Q:Rb|b','S:Qc|c']
 G_format = []
+G_simed = []
+G_output = []
+G_none = []
 for i in range(n):
-    G.append(input())
+    #G.append(input())
     G_format.append([])
     format(i)
 
-print(G_format)
-
-
-#for i in range(1,n + 1):
-#    for j in range(1,i):
+# 显示整理后的结果
+# print(G_format)
+for i in range(n):
+    for j in range(i):
+        remove_indir(i,j)
+simpli(i)
+remove_direc()
+print(G_none)
+print(G_output)
         # 把形如Pi→Pjγ的规则改写成
         # Pi→δ1γ|δ2γ|……|δkγ。其中Pj→δ1|δ2|……|δk是关于Pj的所有规则
     # 消除关于Pi规则的直接左递归性
